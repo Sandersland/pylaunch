4f9c54c6d9f5753a6f660da9aec85cc0c23a82ee
cxa-sandersland <sandersland@connexa.com>
2019-12-28 13:44:10 -0600

    added test case skeleton

---
+++ dial/tests/testDIscoveryAndLaunch.py
@@ -0,0 +1,11 @@
+from dial import DIscoveryAndLaunch
+from unittest import TestCase
+
+class testDIscoveryAndLaunch(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+
