755a8a3e097fb3c4575dedc70fc611b31e2ae3a6
cxa-sandersland <sandersland@connexa.com>
2020-01-07 20:40:58 -0600

    update to gitignore and setup

--- setup.py
+++ setup.py
@@ -6,12 +6,18 @@ with open("README.md", "r") as fh:
 setup(
     name='pylaunch',
     py_modules=['dial','ssdp'],
-    description=long_description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     package_dir={'':'src'},
     version='0.0.99',
     url='https://github.com/Sandersland.dial',
     author="Steffen Andersland",
     author_email='stefandersland@gmail.com',
     keywords=['dial'],
-    install_requires=['requests']
+    install_requires=['requests'],
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows :: Windows 10"
+    ]
 )
